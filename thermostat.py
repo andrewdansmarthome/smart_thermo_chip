b747c23f8ce43674e295dd708235bbbcf0b2da7a
Andrew Trahan - RPi0 <globalart19@gmail.com>
2018-12-15 23:56:14 -0600

    Initial setup and first couple functions written. Definitely need to restructure into multiple files at some point. Goodnight!

--- thermostat.py
+++ thermostat.py
@@ -1,7 +1,94 @@
-import time
-import os
 import RPi.GPIO as GPIO
+import time, threading, requests
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setup(GPIO.BCM)
 
-GPIO.output(PIN, GPIO.LOW)
+# ~~~~~~~~ PIN SETUP ~~~~~~~~
+furnacePin = 17 # pin 11
+tempPin = 27 # pin 13
+ioTestPin = 26 # pin 37
+
+GPIO.setup(furnacePIn, GPIO.OUT)
+GPIO.setup(tempPin, GPIO.IN)
+GPIO.setup(ioTestPin, GPIO.IN)
+
+# ~~~~~~~~ INIT VARIABLES ~~~~~~~~~
+processDelay = 300 # in seconds
+furnaceOn = False
+previousTime = time.time()
+
+# ~~~~~~~~ API URLS ~~~~~~~~~~~~
+urlConfig = 'https://www.google.com'
+
+# ~~~~~~~~ CONFIG VARIABLES ~~~~~~~~~
+config = {
+	"running" = True,
+	"transmitDelay" = 300 # server send delay (in seconds)
+	"targetTemp" = 70 # current target temperature (degrees farenheight)
+	"nextScheduledTime" = 0 # time since epoch for next scheduled action (in seconds)
+	"nextScheduledTemp" = 68 # scheduled temp (deg F)
+}
+
+def initializeApp():
+	# Read from default config file and initialize config dictionary
+
+def updateConfig():
+	initURL = urlConfig
+	res = requests.get(url = initURL)
+	data = res.json()
+	global config = res['config']
+
+def furnaceControl(turnFurnaceOn = furnaceOn):
+	if (turnFurnaceOn):
+		GPIO.output(furnacePin, GPIO.HIGH)
+		furnaceOn = True
+	else:
+		GPIO.output(furnacePin, GPIO.LOW)
+		furnaceOn = False
+
+def readTemp():
+	curVoltage = GPIO.input(tempPin)
+	return convertTemp(curVoltage)
+
+def sendTemp(temp):
+	
+
+def convertTemp(volts):
+	
+
+def readConfig():
+	
+
+def writeConfig():
+	
+
+def scheduler(curTime):
+	if (curTime >= nextScheduledTime):
+		global config
+		config['targetTemp'] = config['nextScheduledTemp']
+		readSchedule(config['nextScheduledTime'])
+
+def readSchedule(curTime):
+	# Read schedule file and return nextScheduledTemp and nextScheduledTime
+
+def ioTest():
+	furnaceControl(not furnaceOn)
+
+# Initialize listener on ioTestPin
+GPIO.add_event_detect(ioTestPin, GPIO.RISING, ioTest())
+
+# Run scheduling process
+while running:
+	global previousTime, config
+	
+	temp = readTemp()
+	curTime = time.time()
+	cycleTime = curTime - previousTime
+	
+	# Run scheduler
+	scheduler(cycleTime)
+	
+	# Send stored temperature data
+	if (cycleTime > transmitDelay):
+		previousTime = time.time()
+		sendTemp()
